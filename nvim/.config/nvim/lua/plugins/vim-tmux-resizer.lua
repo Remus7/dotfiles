40e91ba45d2b820bdec70aa6f86e96bd79a9b780
Remus <remus.rughinis.007@gmail.com>
2025-02-28 23:33:59 +0200

    Fix for vim-tmux-navigator(again)

--- nvim/.config/nvim/lua/plugins/vim-tmux-resizer.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-resizer.lua
@@ -1,10 +1,3 @@
 return {
   "RyanMillerC/better-vim-tmux-resizer",
-  keys = {
-    { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  }
 }
